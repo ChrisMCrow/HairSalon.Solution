8c74346cf2791540bc9bf2c2bfb9ae3387e1d5c4
Chris Crow <chrismcrow@gmail.com>
2018-09-21 12:16:59 -0700

    write find function and pass test

--- HairSalon.Tests/ModelTests/StylistTest.cs
+++ HairSalon.Tests/ModelTests/StylistTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using HairSalon.Models;
 
 namespace HairSalon.Tests
@@ -15,6 +16,46 @@ namespace HairSalon.Tests
         public StylistTest()
         {
             DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=chris_crow_test;";
+        }
+
+        [TestMethod]
+        public void Equals_ReturnsTrueForSameNameAndId_True()
+        {
+            //Arrange, Act
+            Stylist firstStylist = new Stylist("Jane Doe");
+            Stylist secondStylist = new Stylist("Jane Doe");
+
+            //Assert
+            Assert.AreEqual(firstStylist, secondStylist);
+        }
+
+        [TestMethod]
+        public void Save_SavesStylistToDatabase_StylistList()
+        {
+            //Arrange
+            Stylist testStylist = new Stylist("Freddy Mercury");
+
+            //Act
+            testStylist.Save();
+            List<Stylist> databaseList = Stylist.GetAll();
+
+            //Assert
+            Assert.AreEqual(testStylist, databaseList[0]);
+        }
+
+        [TestMethod]
+        public void Find_DatabaseFindsObjectById_StylistObject()
+        {
+            //Arrange
+            Stylist testStylist = new Stylist ("Joan Jett");
+            testStylist.Save();
+            int testId = testStylist.GetStylistId();
+
+            //Act
+            Stylist result = Stylist.Find(testId);
+
+            //Assert
+            Assert.AreEqual(result, testStylist);
         }
     }
 }
