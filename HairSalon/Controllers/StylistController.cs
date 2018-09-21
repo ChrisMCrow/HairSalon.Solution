332f0e13195b2f5fe46e4d1152a2bdb62c4bf9b9
Chris Crow <chrismcrow@gmail.com>
2018-09-21 11:13:46 -0700

    add basic model, controller, and view.

---
+++ HairSalon/Controllers/StylistController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+    public class StylistController : Controller
+    {
+        [HttpPost("/stylist/new")]
+        public ActionResult Create(string stylistName)
+        {
+            Stylist newStylist = new Stylist(stylistName);
+            newStylist.Save();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -7,6 +7,104 @@ namespace HairSalon.Models
 {
     public class Stylist
     {
+        private int _stylistId;
+        private string _stylistName;
 
+        public Stylist(string name, int id = 0)
+        {
+            _stylistName = name;
+            _stylistId = id;
+        }
+
+        public int GetStylistId()
+        {
+            return _stylistId;
+        }
+        public string GetStylistName()
+        {
+            return _stylistName;
+        }
+
+        public override bool Equals(System.Object otherStylist)
+        {
+            if(!(otherStylist is Stylist))
+            {
+                return false;
+            }
+            else
+            {
+                Stylist newStylist = (Stylist) otherStylist;
+                bool areNamesEqual = this.GetStylistName().Equals(newStylist.GetStylistName());
+                bool areIdsEqual = this.GetStylistId().Equals(newStylist.GetStylistId());
+                return (areNamesEqual && areIdsEqual);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.GetStylistName().GetHashCode();
+        }
+
+        public static void ClearAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE from stylists;";
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO stylists (stylist_name) VALUES (@stylistName);";
+
+            cmd.Parameters.Add(new MySqlParameter("@stylistName", this._stylistName));
+
+            cmd.ExecuteNonQuery();
+            _stylistId = (int) cmd.LastInsertedId;
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public static List<Stylist> GetAll()
+        {
+            List<Stylist> allStylists = new List<Stylist> {};
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM stylists;";
+
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+            int id = 0;
+            string name = "";
+            while (rdr.Read())
+            {
+                id = rdr.GetInt32(0);
+                name = rdr.GetString(1);
+                Stylist newStylist = new Stylist(name, id);
+                allStylists.Add(newStylist);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allStylists;
+        }
     }
 }
